e25db6bb1dfedbdbda42f8ebad8a9508a28c1774
Sobhan ghasemi <mohammadghasemi3033@gmail.com>
2024-11-10 23:53:00 +0330

    style : fixing some dark mode style bugs

--- resources/views/Foods/create.blade.php
+++ resources/views/Foods/create.blade.php
@@ -1,13 +1,13 @@
 <x-app-layout>
     <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8 w-full h-screen flex justify-center items-center">
-        <form action="{{route('foods.store')}}" method="POST" class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8 shadow-md flex flex-col rounded-md">
+        <form action="{{route('foods.store')}}" method="POST" class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8 shadow-md flex flex-col rounded-md dark:bg-gray-900">
             @csrf
 
             <x-input-label>{{__('What\'s your food name?')}}</x-input-label>
-            <input type="text" name="Name" class="mb-5">
+            <input type="text" name="Name" class="mb-5 dark:bg-gray-700 dark:outline-0 dark:border-none">
 
             <x-input-label>{{__('How much is it?')}}</x-input-label>
-            <input type="text" name="Price" class="mb-5">
+            <input type="text" name="Price" class="mb-5 dark:bg-gray-700 dark:outline-0 dark:border-none">
 
             <div class="w-full flex justify-between">
                 <x-primary-button>{{__('Save')}}</x-primary-button>
--- resources/views/Foods/edit.blade.php
+++ resources/views/Foods/edit.blade.php
@@ -1,13 +1,13 @@
 <x-app-layout>
-    <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8 w-full h-screen flex justify-center items-center">
-        <form action="{{route('foods.update' , $food->id)}}" method="POST" class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8 shadow-md flex flex-col rounded-md">
+    <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8 w-full h-screen flex justify-center items-center ">
+        <form action="{{route('foods.update' , $food->id)}}" method="POST" class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8 shadow-md flex flex-col rounded-md dark:bg-gray-900">
             @csrf
             @method("PUT")
             <x-input-label>{{__('What\'s your food name?')}}</x-input-label>
-            <input type="text" name="Name" class="mb-5" value="{{$food["Name"]}}">
+            <input type="text" name="Name" class="mb-5 dark:bg-gray-700 dark:outline-0 dark:border-none" value="{{$food["Name"]}}">
 
             <x-input-label>{{__('How much is it?')}}</x-input-label>
-            <input type="text" name="Price" class="mb-5" value="{{$food["Price"]}}">
+            <input type="text" name="Price" class="mb-5 dark:bg-gray-700 dark:outline-0 dark:border-none" value="{{$food["Price"]}}">
 
             <div class="w-full flex justify-between">
                 <x-primary-button>{{__('Save')}}</x-primary-button>
